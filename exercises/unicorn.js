9252530affc1334547255a4f8899b65c24e1c453
John Adams <johnadams@Johns-MacBook-Pro.local>
2019-08-25 15:04:43 -0600

    Finish vampire mythical critters

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,11 +1,11 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    if (color === undefined) {
-      this.color = 'white';
-    } else {
-    this.color = color;
-    }
+    // if (color === undefined) {
+    //   this.color = 'white';
+    // } else {
+    // this.color = color;
+    this.color = color || "white"
   }
     isWhite() {
       if(this.color === 'white') {
