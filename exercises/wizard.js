86eae2faf363838a04d5e24e1537c5b635bdc97c
John Adams <johnadams@Johns-MacBook-Pro.local>
2019-08-28 09:44:15 -0600

    Complete Wizard mythical critter

--- exercises/wizard.js
+++ exercises/wizard.js
@@ -1,8 +1,27 @@
 class Wizard {
-  constructor(someName) {
-    this.name = someName.theObject;
+  constructor(obj) {
+    this.name = obj.name;
+    if (obj.bearded === undefined) {
+      this.bearded = true;
+    } else {
+      this.bearded = obj.bearded;
+    }
+    this.isRested = true;
+    this.castCount = 0;
   }
-}
+  incantation(text) {
+    return text.toUpperCase();
+  }
+  cast() {
+    this.castCount += 1;
+    if (this.castCount > 2) {
+      this.isRested = false;
+      return 'I SHALL NOT CAST';
+    } else {
+      return 'MAGIC BULLET';
+    }
+  }
+  }
 
 
 module.exports = Wizard;
