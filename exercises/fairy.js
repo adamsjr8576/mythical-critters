e8b30024fcbc5583498a058843a332bfa1262877
John Adams <adamsjr8576@gmail.com>
2019-09-12 22:28:57 -0600

    Finish direwolf and fairy

---
+++ exercises/fairy.js
@@ -0,0 +1,35 @@
+class Fairy {
+  constructor(name) {
+    this.name = name;
+    this.dust = 10;
+    this.clothes = {dresses: ['Iris']};
+    this.disposition = 'Good natured';
+    this.humanWards = [];
+  }
+  receiveBelief() {
+    this.dust ++;
+  }
+  believe() {
+    this.dust += 10;
+  }
+  makeDresses(flowers) {
+    this.clothes.dresses = this.clothes.dresses.concat(flowers);
+  }
+  provoke() {
+    this.disposition = `Vengeful`
+  }
+  replaceInfant(child) {
+    if(this.disposition === `Vengeful`) {
+      child.disposition = `Malicious`;
+      this.humanWards.push(child);
+      console.log(this.humanWards.length);
+    } else {
+      return child;
+    }
+    if (this.humanWards.length > 2) {
+      this.disposition = 'Good natured';
+    }
+  }
+  }
+
+module.exports = Fairy;
